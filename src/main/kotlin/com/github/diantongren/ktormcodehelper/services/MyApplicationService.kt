568d8b5c1f7a47d6bd3adf3ccda1ea3e2f476abe
GitHub Action <action@github.com>
2022-10-17 05:54:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/diantongren/ktormcodehelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/diantongren/ktormcodehelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.diantongren.ktormcodehelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.diantongren.ktormcodehelper.MyBundle
 
 class MyApplicationService {
 
